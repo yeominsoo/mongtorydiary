e82fe93d42d8920aadb2a7152d4eb8753f4b9cf2
yeominsoo <dualstn4798@gmail.com>
2021-06-13 21:39:18 +0900

    first

---
+++ src/test/java/com/mongtory/diary/MongtoryDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mongtory.diary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongtoryDiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
