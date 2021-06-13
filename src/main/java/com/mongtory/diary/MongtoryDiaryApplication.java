ab0c465ca4e6647db74caae12d1257cfa508e017
yeominsoo <dualstn4798@gmail.com>
2021-06-13 19:51:55 +0900

    first

---
+++ src/main/java/com/mongtory/diary/MongtoryDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.mongtory.diary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongtoryDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongtoryDiaryApplication.class, args);
+	}
+
+}
