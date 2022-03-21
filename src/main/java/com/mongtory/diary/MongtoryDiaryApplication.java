4bc664e49d2e014c30919f3bbe96a0ff7207685f
yeominsoo <dualstn4798@gmail.com>
2022-03-21 11:37:52 +0900

    first

--- src/main/java/com/mongtory/diary/MongtoryDiaryApplication.java
+++ src/main/java/com/mongtory/diary/MongtoryDiaryApplication.java
@@ -1,7 +1,14 @@
 package com.mongtory.diary;
 
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 @SpringBootApplication
 public class MongtoryDiaryApplication {
@@ -9,5 +16,15 @@ public class MongtoryDiaryApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MongtoryDiaryApplication.class, args);
 	}
-
+    
+	@Bean
+	public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+		SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+		sessionFactory.setDataSource(dataSource);
+		
+		Resource[] res = new PathMatchingResourcePatternResolver().getResources("classpath:mappers/*Mapper.xml");
+		sessionFactory.setMapperLocations(res);
+		
+		return sessionFactory.getObject();
+	}
 }
